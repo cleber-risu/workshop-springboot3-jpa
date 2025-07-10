ca1cf0bfeb51f5307447aa2aca4c047cfd1e93ab
cleber <cleber.risu@outlook.com>
2025-07-10 11:43:53 -0300

    User update

--- src/main/java/com/edu/course/services/UserService.java
+++ src/main/java/com/edu/course/services/UserService.java
@@ -31,4 +31,18 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		// diferente do findById ele prepara o obj monitado para alteração
+		// sendo mais eficiente
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
